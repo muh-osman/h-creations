1eb2d49ab28c6305737b223d0bbf6b9c502de17c
muh-osman <mr.muhammed.osman@gmail.com>
2024-02-04 22:44:59 +0300

    Fix

--- src/pages/Marketing/Marketing.jsx
+++ src/pages/Marketing/Marketing.jsx
@@ -13,6 +13,12 @@ import servC from "../../assets/images/markiting/Influencer.png";
 import servD from "../../assets/images/markiting/social.png";
 import servE from "../../assets/images/markiting/email.png";
 
+import inful from "../../assets/images/markiting/Influ.webp";
+
+import soc from "../../assets/images/markiting/soc.webp";
+
+import mail from "../../assets/images/markiting/mail.png";
+
 export default function Marketing() {
   useEffect(() => {
     window.scroll({
@@ -112,6 +118,98 @@ export default function Marketing() {
           </div>
           <div>
             <img src={seo} alt="seo" />
+          </div>
+        </div>
+      </section>
+
+      <section className={style.D}>
+        <h1>التسويق عبر الإعلانات الرقمية</h1>
+
+        <div className={style.D_box}>
+          <div>
+            <h2>أهمية الإعلانات الرقمية (Google Ads) في توسيع نشاطك:</h2>
+            <h6>
+              وضع خطة واستراتيجية الخاصة بـ الإعلانات الرقمية المدفوعة مهمة
+              كبيرة لأنه يلزمها دراسة جيدة دقيقة للبيانات Data Analysis لوضع
+              الشركة او النشاط الحالي ووضعها مقارنة بالمنافسين بالسوق بالإضافة
+              إلى دراسة القنوات الإعلانية للسوشيال ميديا أو جوجل ادز وتأثيرها
+              على الجمهور المستهدف ومدى التفاعل عليها ونتائجها للهدف الرئيسي ما
+              ترتب عليها في توزيع تكلفة الإعلانات.
+            </h6>
+            <Link to="/contact">اطلب الآن</Link>
+          </div>
+          <div>
+            <iframe
+              width={560}
+              height={315}
+              src="https://www.youtube.com/embed/e8wJBq6vOAI?si=LgcI_fcexWsER2KJ"
+              title="YouTube video player"
+              frameBorder={0}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className={style.B}>
+        <h1>التسويق عبر المؤثرين</h1>
+
+        <div className={style.B_box}>
+          <div>
+            <h2>حقق الانتشار لأعمالك من خلال التسويق بالمشاهير</h2>
+            <h6>
+              التسويق عبر المؤثرين تعتبر الآن من أفضل الطرق لزيادة الوعي بعلامتك
+              التجارية والنمو السريع والوصول لجمهورك المستهدف فمن خلالها يمكنك
+              زيادة عدد المتابعين والتفاعل المستمر معهم على منصات التواصل
+              الاجتماعي المختلفة.
+            </h6>
+            <Link to="/contact">اطلب الآن</Link>
+          </div>
+          <div>
+            <img src={inful} alt="seo" />
+          </div>
+        </div>
+      </section>
+
+      <section className={style.E}>
+        <h1>التسويق عبر وسائل التواصل الاجتماعي</h1>
+
+        <div className={style.E_box}>
+          <div>
+            <h2>تفاعل مع عملائك من خلال التسويق عبر وسائل التواصل الاجتماعي</h2>
+            <h6>
+              تعتبر منصات السوشيال ميديا التي يستخدمها أكثر من 8 مليار مستخدم في
+              العالم نافذة للتخاطب بين الشركات أو المؤثرين والجمهور ومن خلالها
+              يمكن بناء قاعدة كبيرة من العملاء المحتملين لك وتسهل التواصل مع
+              العملاء الحاليين.
+            </h6>
+            <Link to="/contact">اطلب الآن</Link>
+          </div>
+          <div>
+            <img src={soc} alt="seo" />
+          </div>
+        </div>
+      </section>
+
+      <section className={style.B}>
+        <h1>حملات البريد الالكتروني</h1>
+
+        <div className={style.B_box}>
+          <div>
+            <h2>أقوى وأنجح الوسائل التقليدية والحديثة في الآن ذاته</h2>
+            <h6>
+              رغم أن التسويق عبر البريد الإلكتروني (Email Marketing) يعد من أقدم
+              وسائل التسويق الإلكتروني، إلا أنه يعتبر من أقوى قنوات التسويق
+              وأفضلها! وذلك بسبب قدرته على الوصول إلى أكبر قدر من العملاء
+              المهتمين الذين ترتبط بياناتهم وبريدهم الإلكتروني معك منذ التعامل
+              الأول. والذي فيما بعد يتيح لك استخدامها لزيادة المبيعات وتقديم
+              خدمة عملاء على أعلى مستويات التخصيص والتخصص.
+            </h6>
+            <Link to="/contact">اطلب الآن</Link>
+          </div>
+          <div>
+            <img src={mail} alt="seo" />
           </div>
         </div>
       </section>
