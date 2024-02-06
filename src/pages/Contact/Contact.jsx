6cb2611006046ca3981fe6a50fb2913bb372eeb2
muh-osman <mr.muhammed.osman@gmail.com>
2024-02-06 23:35:28 +0300

    Fix

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -158,7 +158,7 @@ export default function Contact() {
 
         <div className={style.map_box}>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d927143.4238513196!2d46.7041013!3d24.8072987!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f03890d489399%3A0xba974d1c98e79fd5!2sRiyadh%20Saudi%20Arabia!5e0!3m2!1sen!2s!4v1706866718425!5m2!1sen!2s"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57990.47135128329!2d46.63286177254739!3d24.71299104626764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f03890d489399%3A0xba974d1c98e79fd5!2sRiyadh%20Saudi%20Arabia!5e0!3m2!1sen!2s!4v1707250890233!5m2!1sen!2s"
             width={width > 575 ? width / 2 : 350}
             height={width > 575 ? width / 2.6 : 350}
             style={{ border: 0 }}
